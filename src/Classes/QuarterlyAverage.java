9ad86bfbee06e672949c8865cd996f8c7cb2736c
Andrew <94652124+TalladegaRS6@users.noreply.github.com>
2022-02-03 20:16:29 +0200

    New Update

--- src/Classes/QuarterlyAverage.java
+++ src/Classes/QuarterlyAverage.java
@@ -25,8 +25,6 @@ public class QuarterlyAverage {
             e.printStackTrace();
         }
 
-        System.out.println(infoAboutStudents);
-        System.out.println();
         workWithData();
 
     }
@@ -39,9 +37,9 @@ public class QuarterlyAverage {
             String name = "" ;
             double notaTeza = 0 ;
             StringTokenizer myString = new StringTokenizer(counter,",") ;
+            double finalGrade ;
             while ( myString.hasMoreTokens() ) {
                 String getValue = myString.nextToken() ;
-
                 if ( getValue.endsWith("teza")) {
                     notaTeza = Integer.parseInt(getValue.substring(0, getValue.lastIndexOf("teza")).trim());
                 }else {
@@ -55,9 +53,30 @@ public class QuarterlyAverage {
                 }
 
             }
-            System.out.println(name + " " + ((average/gradePointsCounter)*3+notaTeza)/4);
+            finalGrade = ((average/gradePointsCounter)*3+notaTeza)/4;
+            if ( addContentInFile(name, finalGrade)) {
+                System.out.println("Student was added");
+            }else{
+                System.out.println("ERROR");
+            }
         }
 
+    }
+
+    public boolean addContentInFile ( String studentName, double studentGrade ) {
+
+        boolean isAdded = false ;
+        String studentInformation ="Student name: " + studentName + "\n"
+                                + "Final grade: " + studentGrade + "\n\n";
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter("C:\\WorkWithFiles\\src\\Files\\numeleInitial_medii.txt",true)) ;
+            out.write(studentInformation);
+            out.close();
+            isAdded = true ;
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isAdded ;
     }
 
 
